596cc96bed242f6235fa13319f6474fbb335d6f0
Janarthanan107 <janarthananmuthu107@gmail.com>
2025-01-30 22:56:30 +0000

    .

--- janachat/app/src/main/java/com/example/janachat/Message.kt
+++
@@ -1,7 +0,0 @@
-package com.example.janachat
-
-class Message( var message:String?=null,
-               var senderid:String?=null) {
-
-
-}
--- janachat/app/src/main/java/com/example/janachat/ui/GroupMessage.kt
+++
@@ -1,5 +0,0 @@
-package com.example.janachat.ui
-
-class GroupMessage (val group_id : String?, val message_txt : String?, val time_stamp : Long?) {
-    constructor() : this("", "", 0)
-} // done
